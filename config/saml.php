e5dfaf41e7851f2df86b4df6348b5cf28321ac0f
Rafael Villa Verde <villa655321verde@gmail.com>
2022-02-07 01:11:18 -0300

    fix config

--- config/saml.php
+++ config/saml.php
@@ -7,6 +7,6 @@ return [
     'signin_route' => 'signin',
     'inertia' => true,
     'slo_redirect' =>  env('SLO_REDIRECT', 'autum.com.br'),
-    'autum_acs_url' =>  env('IDP_URL', 'https://accounts-local.com.br') . '/login',
-    'autum_sls_url' =>  env('IDP_URL', 'https://accounts-local.com.br') . '/logout',
+    'autum_acs_url' =>  env('IDP_URL', 'https://accounts-local.autum.com.br') . '/login',
+    'autum_sls_url' =>  env('IDP_URL', 'https://accounts-local.autum.com.br') . '/logout',
 ];
